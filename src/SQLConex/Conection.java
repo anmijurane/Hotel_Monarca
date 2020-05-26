e934465e7a62ba81256081abd138e6a7dfba5a59
Andres Jurado <anmijurane@gmail.com>
2020-05-26 17:58:17 -0500

    Nuevo Archivo para la conexion mysql

---
+++ src/SQLConex/Conection.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package SQLConex;
+
+/**
+ * 
+ * @author anmijurane <miguel.andres_sic@tesco.edu.mx>
+ */
+public class Conection {
+
+}
