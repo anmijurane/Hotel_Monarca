fe04467bbb161e5d8b0e1ebc9d9c0bed6080cbd4
DiegoCardenas <diegocardenas355@gmail.com>
2020-06-10 00:02:06 -0500

    Combo Box agregados

--- src/Service/Limpieza.java
+++ src/Service/Limpieza.java
@@ -48,7 +48,7 @@ public class Limpieza extends javax.swing.JFrame {
         });
         getContentPane().add(FLAT_NUMBER, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 160, 200, 40));
 
-        ROOM_NUMBER.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "101", "102", "103", "104", "105", "106", "107", "108", "109", "110" }));
+        ROOM_NUMBER.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "SELECCIONA UNA HABITACION" }));
         ROOM_NUMBER.setToolTipText(FLAT_NUMBER.getName());
         getContentPane().add(ROOM_NUMBER, new org.netbeans.lib.awtextra.AbsoluteConstraints(580, 160, 200, 40));
 
@@ -65,29 +65,29 @@ public class Limpieza extends javax.swing.JFrame {
             = {"SELECIONA UNA HABITACION", "201", "202", "203", "204", "205", "206", "207", "208",
                 "209", "210"};
     String[] flatTree
-            = {"SELECIONA UNA HABITACION", "201", "202", "203", "204", "205", "206", "207", "208",
-                "209", "210"};
+            = {"SELECIONA UNA HABITACION", "301", "302", "303", "304", "305", "306", "307", "308",
+                "309", "310"};
     String[] flatFour
-            = {"SELECIONA UNA HABITACION", "201", "202", "203", "204", "205", "206", "207", "208",
-                "209", "210"};
+            = {"SELECIONA UNA HABITACION", "401", "402", "403", "404", "405", "406", "407", "408",
+                "409", "410"};
     String[] flatFive
-            = {"SELECIONA UNA HABITACION", "201", "202", "203", "204", "205", "206", "207", "208",
-                "209", "210"};
+            = {"SELECIONA UNA HABITACION", "501", "502", "503", "504", "505", "506", "507", "508",
+                "509", "510"};
     String[] flatSix
-            = {"SELECIONA UNA HABITACION", "201", "202", "203", "204", "205", "206", "207", "208",
-                "209", "210"};
+            = {"SELECIONA UNA HABITACION", "601", "602", "603", "604", "605", "606", "607", "608",
+                "609", "610"};
     String[] flatSeven
-            = {"SELECIONA UNA HABITACION", "201", "202", "203", "204", "205", "206", "207", "208",
-                "209", "210"};
+            = {"SELECIONA UNA HABITACION", "701", "702", "703", "704", "705", "706", "707", "708",
+                "709", "710"};
     String[] flatEight
-            = {"SELECIONA UNA HABITACION", "201", "202", "203", "204", "205", "206", "207", "208",
-                "209", "210"};
+            = {"SELECIONA UNA HABITACION", "801", "802", "803", "804", "805", "806", "807", "808",
+                "809", "810"};
     String[] flatNine
-            = {"SELECIONA UNA HABITACION", "201", "202", "203", "204", "205", "206", "207", "208",
-                "209", "210"};
+            = {"SELECIONA UNA HABITACION", "901", "902", "903", "904", "905", "906", "907", "908",
+                "909", "910"};
     String[] flatTen
-            = {"SELECIONA UNA HABITACION", "201", "202", "203", "204", "205", "206", "207", "208",
-                "209", "210"};
+            = {"SELECIONA UNA HABITACION", "1001", "1002", "1003", "1004", "1005", "1006", "1007", "1008",
+                "1009", "1010"};
               
 
 
@@ -100,7 +100,7 @@ public class Limpieza extends javax.swing.JFrame {
 
             switch (index) {
                 case 0:
-                    System.out.println("Seleciona un piso");
+                    
                     break;
                 case 1:
                     ROOM_NUMBER.setModel(new DefaultComboBoxModel<>(flatOne));
@@ -132,7 +132,6 @@ public class Limpieza extends javax.swing.JFrame {
                 case 10:
                     ROOM_NUMBER.setModel(new DefaultComboBoxModel<>(flatTen));
                     break;
-
                 default:
                     throw new AssertionError();
             }
